2a410f6426eb06e1eaed8d939c1b77ff376bc06e
Marcelo Amarante Cintra <est-marcelo.cintra@tjsp.jus.br>
2024-10-03 12:26:38 -0300

    Alterações no Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,11 +8,11 @@ const Item = ({ name, desc, price, discount, img, isVegan, containsGluten }) => 
 
     return (
         <div>
-            <Card style={{ width: '18rem', height: '28rem' }} className="border-secondary m-3">
+            <Card style={{ width: '18rem', height: '28rem' }} className="m-2 d-flex flex-column">
                 <div className="overflow-hidden rounded-top" style={{ height: '200px' }}>
                     <Card.Img variant="top" style={{ transform: 'translateY(-30px)' }} src={img} />
                 </div>
-                <Card.Body>
+                <Card.Body className="d-flex flex-column flex-grow-1">
                     <Card.Title className="fs-4">{name}</Card.Title>
 
                     <Card.Text className="d-flex justify-content-between align-items-center border-start-0 border-end-0">
@@ -59,9 +59,10 @@ const Item = ({ name, desc, price, discount, img, isVegan, containsGluten }) => 
                     >
                         {desc}
                     </Card.Text>
-                    <div className="d-flex justify-content-between">
+
+                    <div className="mt-auto d-flex justify-content-between align-items-center">
                         <Button onClick={handleShow} variant="primary">Detalhes</Button>
-                        <Button variant="primary"><i className="bi bi-plus-circle"></i></Button>
+                        <Button variant="primary" className="mt-1"><i className="bi bi-plus-circle"></i></Button>
                     </div>
                 </Card.Body>
             </Card>

8426df9aac7a3768f45dd7e8a06ccd069bfbdab2
Marcelo Amarante Cintra <est-marcelo.cintra@tjsp.jus.br>
2024-10-08 10:18:34 -0300

    FormSearch funcional

--- src/components/FormSearch.js
+++ src/components/FormSearch.js
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 import { ListGroup, Form, InputGroup } from 'react-bootstrap';
+import ItemModal from './ItemModal';
 
 const FormSearch = ({ items }) => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredItems, setFilteredItems] = useState([]);
 
+    const nullItem = {
+        "id": 99,
+        "image": "none",
+        "name": "none",
+        "description": "none",
+        "price": 0.0,
+        "discount": 0,
+        "isVegan": false,
+        "containsGluten": true
+    }
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [currentItem, setCurrentItem] = useState(nullItem)
+    const [filteredItems, setFilteredItems] = useState([]);
+    const [show, setShow] = useState(false);
+    const handleShow = (item) => {
+        setCurrentItem(item)
+        setShow(true)
+
+    }
     if (items) {
         const products = [
             ...items.Burgers,
@@ -20,7 +39,7 @@ const FormSearch = ({ items }) => {
 
             if (products && term !== '') {
                 const allItems = Object.values(products).flat();
-                const normalizedTerm = term.normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Remove acentos
+                const normalizedTerm = term.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
                 const results = allItems.filter(item => {
                     const normalizedItemName = item.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
                     return normalizedItemName.toLowerCase().includes(normalizedTerm.toLowerCase());
@@ -32,7 +51,7 @@ const FormSearch = ({ items }) => {
         };
 
         return (
-            <div className="me-2 position-fixed">
+            <div className="me-2">
                 <div>
                     <InputGroup>
                         <Form.Control
@@ -44,23 +63,39 @@ const FormSearch = ({ items }) => {
                         />
                     </InputGroup>
                 </div>
-                {filteredItems.length > 0 && (
-                    <div className='z-3' style={{ maxHeight: '200px', overflowY: 'auto' }}>
-                        <ListGroup>
-                            {filteredItems.map(item => (
-                                <ListGroup.Item key={item.id}>
-                                    <img src={item.image} alt={item.name} width="50" className="me-2" />
-                                    {item.name} - {item.description} - R$ {item.price}
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
-                    </div>
-                )}
+                <div className='position-absolute z-3'>
+                    {filteredItems.length > 0 && (
+                        <div className='z-3' style={{ maxHeight: '200px', overflowY: 'auto', cursor: 'pointer' }}>
+                            <ListGroup>
+                                {filteredItems.map(item => (
+                                    <ListGroup.Item
+                                        onClick={() => handleShow(item)}
+                                        key={item.id}>
+                                        <img src={item.image} alt={item.name} width="50" className="me-2" />
+                                        {item.name} - R$ {(item.price * (100 - item.discount) / 100).toFixed(2)}
+                                    </ListGroup.Item>
+
+                                ))}
+                            </ListGroup>
+                        </div>
+                    )}
+                </div>
+                <ItemModal
+                    show={show}
+                    onHide={() => setShow(false)}
+                    name={currentItem.name}
+                    desc={currentItem.description}
+                    price={currentItem.price}
+                    discount={currentItem.discount}
+                    img={currentItem.image}
+                    isVegan={currentItem.isVegan}
+                    containsGluten={currentItem.containsGluten}
+                />
             </div>
         );
     }
 
-    return null; // Adicione um retorno padrão se `items` não estiver disponível
+    return null;
 }
 
 export default FormSearch;
